78ded76f9923b44d0a5b00e774e37c43db8c1d18
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-02-12 14:22:20 +0800

    experience

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from "@next/font/google";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import WorkExperience from "@/components/WorkExperience";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,6 +32,9 @@ export default function Home() {
       </section>
 
       {/* Experience */}
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
 
       {/* Skills */}
 
