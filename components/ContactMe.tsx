3a85caab2798cbb67de1be5dd336975078cb430c
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-02-13 17:11:48 +0800

    hero icon

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/solid";
 
 type Props = {};
 
@@ -14,6 +15,21 @@ function ContactMe({}: Props) {
           I have got just what you need. {""}
           <span className="decoration-[#F7AB0A]/50 underline">Lets Talk.</span>
         </h4>
+
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">+6512345678</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">test@gmail.com</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">123 st developer</p>
+          </div>
+        </div>
       </div>
     </div>
   );
