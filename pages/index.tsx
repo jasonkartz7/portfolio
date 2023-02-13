844aeedeb4fd1b82bf6fff1004f2d2661a2f3514
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-02-13 16:23:28 +0800

    contact

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import About from "@/components/About";
 import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
+import ContactMe from "@/components/ContactMe";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -49,6 +50,9 @@ export default function Home() {
       </section>
 
       {/* Contact Me */}
+      <section id="contact" className="snap-center">
+        <ContactMe />
+      </section>
     </div>
   );
 }
