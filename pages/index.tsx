b0fc6081b5c58fd49f8cf3338285efeff9a47ab3
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-02-13 11:46:41 +0800

    skills

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import WorkExperience from "@/components/WorkExperience";
+import Skills from "@/components/Skills";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -37,6 +38,9 @@ export default function Home() {
       </section>
 
       {/* Skills */}
+      <section id="skills" className="snap-center">
+        <Skills />
+      </section>
 
       {/* Projects */}
 
