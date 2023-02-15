2842772fc23058e48d797a14beb29011b886894e
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-02-15 12:13:21 +0800

    Merge pull request #19 from jasonkartz7/contact
    contact

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -6,7 +6,7 @@ type Props = {};
 function ContactMe({}: Props) {
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className=" top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Contact
       </h3>
 
@@ -30,6 +30,16 @@ function ContactMe({}: Props) {
             <p className="text-2xl">123 st developer</p>
           </div>
         </div>
+
+        <form action="" className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex space-x-2">
+            <input placeholder="Name" type="text" className="contactInput" />
+            <input placeholder= "Email"type="text" className="contactInput" />
+          </div>
+          <input placeholder="Subject" type="text" className="contactInput" />
+          <textarea placeholder="Message" className="contactInput" />
+          <button className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg">Submit</button>
+        </form>
       </div>
     </div>
   );
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -10,15 +10,16 @@ function Skills({}: Props) {
       className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] 
                             xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="top-24 uppercase tracking-[20px] text-gray-500 text-2xl mx-5">
         Skills
       </h3>
 
-      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
-        Hover over a skill for currency profieciency
-      </h3>
-
-      <div className="grid grid-cols-4 gap-5">
+      <div
+        className="grid grid-cols-4 gap-5 flex flex-col rounded-lg items-center space-y-6 flex-shrink-0
+      w-[500px] md:w-[500px] xl:w-[900px] snap-center bg-[#292929] p-1
+      hover:opacity-100 opacity-40 cursor-pointer transition-opacity
+      duration-200 overflow-hidden"
+      >
         <Skill />
         <Skill />
         <Skill />
